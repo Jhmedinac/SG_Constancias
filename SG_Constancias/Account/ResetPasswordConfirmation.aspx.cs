0768104bc55eaf5309139f984880fbfa56cbf9f6
Javier Medina <jhmedina@tsc.gob.hn>
2024-05-22 15:48:57 -0600

    Agregar archivos de proyecto.

---
+++ SG_Constancias/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using SG_Constancias.Models;
+
+namespace SG_Constancias
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
